d10f4129ae4b39778f73a4f1c423e13a0fa9b282
nima <linux.vps.vpn@gmail.com>
2023-12-16 01:23:02 +0330

    all changes

---
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class UserEntity{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Column(name = "role")
+    private Set<String> roles;
+
+
+}
--- src/main/java/com/example/demo/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String msg) {
-        super(msg);
-    }
-
-}
